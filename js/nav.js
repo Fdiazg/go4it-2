02c33ec5840dd88320b3447499d14c6b7f8f2b35
Felipe Díaz Garay <f.diazgaray90@outlook.com>
2023-08-09 22:59:01 -0400

    [ADD] Footer y form

--- js/nav.js
+++ js/nav.js
@@ -4,3 +4,23 @@ hamburgerMenu.addEventListener('click', () => {
     hamburgerMenu.classList.toggle('active');
 });
 
+const enlacesScroll = document.querySelectorAll('.enlace-scroll');
+
+
+enlacesScroll.forEach(enlace => {
+  enlace.addEventListener('click', function(event) {
+    event.preventDefault();
+    
+    const targetId = this.getAttribute('href');
+    const target = document.querySelector(targetId);
+
+    const headerHeight = document.querySelector('.header-go4it').offsetHeight;
+
+    const offset = target.getBoundingClientRect().top - headerHeight;
+
+    window.scrollTo({
+      top: window.scrollY + offset,
+      behavior: 'smooth'
+    });
+  });
+});
