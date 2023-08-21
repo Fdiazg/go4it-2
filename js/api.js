30ca063ae3b379483e64f41d544b2cc1cd5e2cf8
Felipe Díaz Garay <f.diazgaray90@outlook.com>
2023-08-20 23:34:00 -0400

    [ADD]Version Mobile

--- js/api.js
+++ js/api.js
@@ -1,25 +1,60 @@
 
 // TODO API PAISES
 
- document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', () => {
 
-     const seleccionPaises = document.querySelector('#pais');
-     // https://restcountries.com/v3.1/all
-     fetch('https://restcountries.com/v2/all')
+    const seleccionPaises = document.querySelector('#pais');
+    // https://restcountries.com/v3.1/all
+    fetch('https://restcountries.com/v2/all')
 
-         .then(res => {
-             return res.json();
-         })
-         .then(data => {
-             let output = '';
-             output = `<option disabled selected value=""> Seleccione su país</option> `
-             data.forEach(pais => {
-                 output += `<option value="${pais.name}"> ${pais.name} </option>`;
-             });
-             seleccionPaises.innerHTML = output;
-         })
-         .catch(err => {
-             console.log(err);
-         })
+        .then(res => {
+            return res.json();
+        })
+        .then(data => {
+            let output = '';
+            output = `<option disabled selected value=""> Seleccione su país</option> `
+            data.forEach(pais => {
+                output += `<option value="${pais.name}"> ${pais.name} </option>`;
+            });
+            seleccionPaises.innerHTML = output;
+        })
+        .catch(err => {
+            console.log(err);
+        })
 
- });
+});
+
+
+
+
+// function initMap() {
+//     // Crea un objeto de mapa y configura las opciones
+//     var mapOptions = {
+//         center: { lat: 52.6646, lng: -8.6234 }, // Coordenadas de Limerick
+//         zoom: 12, // Nivel de zoom
+//     };
+//     var map = new google.maps.Map(document.getElementById('mapa-footer'), mapOptions);
+
+//     // Crea un marcador en la localidad de Limerick
+//     var markerOptions = {
+//         position: { lat: 52.6646, lng: -8.6234 }, // Coordenadas de Limerick
+//         map: map,
+//         title: 'Limerick'
+//     };
+//     var marker = new google.maps.Marker(markerOptions);
+// }
+
+
+
+// TODO API MAPA
+
+
+var mapa = L.map('mapa').setView([52.6641, -8.6236], 10); // Latitud, Longitud, y nivel de zoom
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(mapa);
+
+L.marker([52.6641, -8.6236]).addTo(mapa)
+    .bindPopup('Limerick, Irlanda')
+    .openPopup();
--- js/nav.js
+++ js/nav.js
@@ -3,17 +3,17 @@ const hamburgerMenu = document.querySelector(".hamburger-menu");
 const navMenu = document.querySelector('.nav-go4it')
 
 hamburgerMenu.addEventListener('click', () => {
-    hamburgerMenu.classList.toggle('active');
-    navMenu.classList.toggle('nav-open')
+  hamburgerMenu.classList.toggle('active');
+  navMenu.classList.toggle('nav-open')
 });
 
 const enlacesScroll = document.querySelectorAll('.enlace-scroll');
 
 
 enlacesScroll.forEach(enlace => {
-  enlace.addEventListener('click', function(event) {
+  enlace.addEventListener('click', function (event) {
     event.preventDefault();
-    
+
     const targetId = this.getAttribute('href');
     const target = document.querySelector(targetId);
 
@@ -27,3 +27,53 @@ enlacesScroll.forEach(enlace => {
     });
   });
 });
+
+
+
+
+
+
+// document.addEventListener("DOMContentLoaded", function () {
+//   // Verifica la resolución de la pantalla
+//   if (window.innerWidth < 760) {
+//     // Encuentra todos los marcadores de posición
+//     var marcadoresDePosicion = document.querySelectorAll(".contenedor-pais-placeholder");
+
+//     // Itera a través de los marcadores de posición
+//     marcadoresDePosicion.forEach(function (placeholder) {
+//       // Crea un nuevo contenedor para cada marcador de posición
+//       var contenedorPais = document.createElement("div");
+//       contenedorPais.className = "contenedor-pais";
+
+//       // Encuentra el ul que está dentro del mismo elemento padre que el marcador de posición
+//       var ulSubSubMenu = placeholder.nextElementSibling;
+
+//       // Mueve el ul dentro del nuevo contenedor
+//       contenedorPais.appendChild(ulSubSubMenu);
+
+//       // Inserta el nuevo contenedor antes del marcador de posición
+//     placeholder.parentNode.replaceChild(contenedorPais, placeholder);
+//     });
+//   }
+// });
+
+
+
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+
+  const enlacesIrlanda = document.querySelectorAll(".sub-menu-link");
+
+  enlacesIrlanda.forEach(function (enlace) {
+    enlace.addEventListener("click", function (e) {
+      e.preventDefault();
+      let contenedorPais = this.nextElementSibling;
+
+      contenedorPais.classList.toggle("active");
+    });
+  });
+});
+
+
